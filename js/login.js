ffdfd53091387c169f25a0aa0cd5b9988998fc9e
kevinwalkey <72392475+kevinwalkey@users.noreply.github.com>
2020-11-22 18:53:22 -0300

    finalização css

---
+++ js/login.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    function validform() {
+
+        $('#b1').click(function () {
+            let email = $('#email').text();
+
+            console.log(email)
+
+        });
+    }
+
+});
